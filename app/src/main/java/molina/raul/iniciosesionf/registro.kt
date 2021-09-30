0319e91db39c6598d0f699dceec4f209998c2135
ruloxd14 <57108962+ruloxd14@users.noreply.github.com>
2021-09-30 15:49:54 -0700

    Pantallas principales

---
+++ app/src/main/java/molina/raul/iniciosesionf/registro.kt
@@ -0,0 +1,11 @@
+package molina.raul.iniciosesionf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
